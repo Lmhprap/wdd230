e0f196bd5e6d4f6baaf3479ed0f3feceeb1b65ad
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-10-06 14:01:15 -0600

    chamber upload

---
+++ chamber/scripts/hbutton.js
@@ -0,0 +1,7 @@
+const navigation = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
