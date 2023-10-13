ac9f4fa9a873cd9c8287fe3326204a29b5af1302
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-10-12 20:17:38 -0600

    Added DOM requirements

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -1,24 +1,36 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
 
 button.onclick = function() {
     let myItem = input.value;
-    input.value = '';
+    input.value = "";
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
 
     listItem.appendChild(listText);
     listText.textContent = myItem;
     listItem.appendChild(listBtn);
-    listBtn.textContent = '❌';
+    listBtn.textContent = "❌";
     list.appendChild(listItem);
 
-    listBtn.onclick = function(e) {
+    listBtn.onclick = function() {
         list.removeChild(listItem);
+        input.focus();
     }
 
     input.focus();
+
+    const inputvalue = () => {
+        if (myItem == null || myItem.length == 0 || /^\s+$/.test(myItem)) {
+            alert('[ERROR] The form field cannot be empty');
+            list.removeChild(listItem);
+            return false;
+        }
+    }
+
+    inputvalue();
 }
