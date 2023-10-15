5cbbba0197cecd4bdfb861a03a46baa98d5200a5
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-10-14 20:13:43 -0600

    darkmode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -2,13 +2,13 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
+    if (modeButton.textContent.includes("⚫")) {
         main.style.background = "#000";
         main.style.color = "#fff";
-        modeButton.textContent = "🔆";
+        modeButton.textContent = "⚪";
     } else {
         main.style.background = "#eee";
         main.style.color = "#000";
-        modeButton.textContent = "🕶️";
+        modeButton.textContent = "⚫";
     }
 });
