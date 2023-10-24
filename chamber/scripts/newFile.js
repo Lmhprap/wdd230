31eb472caf6e57bf12afb9583dea605dc9291d09
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-10-24 02:13:11 -0600

    changes and cleanup

---
+++ chamber/scripts/newFile.js
@@ -0,0 +1,6 @@
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = "Welcome!Please let us know if you have any questions.";
+}
