9acbd2cabf586f283f190f0076a3750ab71736c9
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-10-25 21:12:23 -0600

    changes

---
+++ lesson03/lazyload/scripts/script.js
@@ -0,0 +1,15 @@
+try {
+    let options = {
+        year: "numeric",
+    };
+    document.querySelector("#copyright").textContent =
+        new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error with copyright year.");
+}
+
+try {
+    document.getElementById("lastmodified").textContent = document.lastModified;
+} catch (e) {
+    alert("Error finding last modified date.");
+}
