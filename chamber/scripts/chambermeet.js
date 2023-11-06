2f116852ebad91470fc26f2b2248089f94e4904b
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-11-05 21:36:47 -0700

    chamber error fixes

--- chamber/scripts/chambermeet.js
+++ chamber/scripts/chambermeet.js
@@ -5,3 +5,11 @@ const element = document.getElementById("chambermeet");
 if (today === 1 || today === 2) {
     element.style.display = "block";
 }
+
+// Script for closing the banner
+const closeBtn = document.querySelector('#bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
