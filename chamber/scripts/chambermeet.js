47566fd77fd1d420bb30f1dc5ea5c6a73fd95561
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-11-06 16:53:17 -0700

    form correction and banner close

--- chamber/scripts/chambermeet.js
+++ chamber/scripts/chambermeet.js
@@ -1,13 +1,13 @@
 // Chamber meeting messages display Monday and Tuesday
 
-const element = document.getElementById("chambermeet");
+const banner = document.querySelector('#chambermeet');
 
 if (today === 1 || today === 2) {
     element.style.display = "block";
 }
 
 // Script for closing the banner
-const closeBtn = document.querySelector('#bannerBtn');
+const closeBtn = document.querySelector('.bannerBtn');
 closeBtn.addEventListener('click', function() {
     if (banner.style.display !== "none") {
         banner.style.display = "none";
--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -39,3 +39,26 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 
 x.onclick = toggleMenu;
+
+// Chamber meeting messages display Monday and Tuesday
+
+const element = document.getElementById("chambermeet");
+
+if (today === 1 || today === 2) {
+    element.style.display = "block";
+}
+
+// Script for closing the banner
+const banner = document.querySelector('#chambermeet');
+
+if (today === 1 || today === 2) {
+    element.style.display = "block";
+}
+
+// Script for closing the banner
+const closeBtn = document.querySelector('.bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
