9088c13e57c1ef3a7287083d270c62d967b7574f
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-12-05 14:16:03 -0700

    II

--- scoots/scripts/toggle.js
+++ scoots/scripts/toggle.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+//To display current date 
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
